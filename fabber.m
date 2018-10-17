c446cd5a43baf26ee549ea892c208fd354f155a0
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-10-17 09:55:52 +0100

    Support boolean options better and separate options using semicolons not commmas

--- fabber.m
+++ fabber.m
@@ -16,16 +16,33 @@ d = double(data);
 % The mask must be logical type
 m = logical(mask);
 
-% Load model libraries by passing them via the rundata as a comma separated list
+% Load model libraries by passing them via the rundata as a semicolon 
+% separated list
 fsldir = getenv('FSLDIR');
 rundata.loadmodels = '';
 fabber_libs = dir([fsldir lib_subdir name_prefix '*' name_suffix]);
 for l = 1:length(fabber_libs)
     file_name = fabber_libs(l).name;
     if (length(rundata.loadmodels) > 0)
-      rundata.loadmodels = [rundata.loadmodels ',']
+      rundata.loadmodels = [rundata.loadmodels ';'];
     end
     rundata.loadmodels = [rundata.loadmodels fsldir lib_subdir file_name];
+end
+
+% Boolean options can be specified as an empty string=true, this is the
+% Fabber expectation, however we also accept logicals (but not 0, 1 because
+% these might be valid option values)
+args = fieldnames(rundata);
+for i = 1:numel(args)
+  arg = args{i};
+  val = rundata.(arg);
+  if islogical(val);
+      if val
+          rundata.(arg) = '';
+      else
+          rundata = rmfield(rundata, arg);
+      end
+  end
 end
 
 output = fabber_wrapper(d, m, rundata);
