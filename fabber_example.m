d25f613dedf43bb70385a44589d1cbaa95c215e0
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-04-03 09:14:40 +0100

    Renamed

--- fabber_example.m
+++ fabber_example.m
@@ -1,9 +1,11 @@
-rundata.data = ones(2, 3, 4, 5);
-rundata.mask = ones(2, 3, 4);
+% Example usage of the Fabber-Matlab API 
+rundata.data = rand(10, 10, 10, 5);
+rundata.mask = ones(10, 10, 10);
 rundata.model='poly';
 rundata.degree = 3;
 rundata.method='vb';
 rundata.noise='white';
+rundata.save_mean = true;
 
 output = fabber(rundata)
 
